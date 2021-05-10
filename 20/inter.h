2be2e973aba40eda9c7ba077594301744a1af98f
kimnanhee <kimnanhee0225@gmail.com>
2021-05-10 23:30:19 +0900

    [Delete] 20 folder

--- 20/inter.h
+++
@@ -1,7 +0,0 @@
-#ifdef __inter_h
-#define __inter_h
-
-void interface_action(void);
-char* interface_information(int mode);
-
-#endif
--- 20/network.h
+++
@@ -1,7 +0,0 @@
-#ifdef __network_h
-#define __network_h
-
-void error_handling(char *msg);
-int socket_init(void);
-
-#endif
